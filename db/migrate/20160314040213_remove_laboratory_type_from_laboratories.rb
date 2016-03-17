0bb6f6e48197173aa36adba7203952f770633057
tmac2470 <taniamachet@gmail.com>
2016-03-17 11:57:33 +1100

    Migrations changed for new structure

--- db/migrate/20160314040213_remove_laboratory_type_from_laboratories.rb
+++ db/migrate/20160314040213_remove_laboratory_type_from_laboratories.rb
@@ -2,7 +2,5 @@ class RemoveLaboratoryTypeFromLaboratories < ActiveRecord::Migration
   def change
     remove_column :laboratories, :laboratory_type, :integer
     remove_column :laboratories, :laboratory_hash, :character
-    remove_column :laboratories, :default, :character
-    remove_column :laboratories, :false, :character
   end
 end
