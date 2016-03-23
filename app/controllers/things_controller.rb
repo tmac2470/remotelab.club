f209383a6d900f248f30daf5f14e216c3e66a511
tmac2470 <taniamachet@gmail.com>
2016-03-23 13:23:41 +1100

    Change for continuous log

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,7 +1,7 @@
 class ThingsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_thing, only: [:show, :edit, :update, :destroy, :ui_designer]
-
+  
   respond_to :html
 
   def index
@@ -10,6 +10,8 @@ class ThingsController < ApplicationController
   end
 
   def show
+  	#@topics = @thing.thing_logs.order("created_at DESC").map(&:topic)
+	@topics = Thing.find(params[:id]).thing_logs.includes(:thing_logs).find_by_sql("select topic, max(updated_at) as latest from thing_logs group by topic")
     respond_with(@thing)
   end
 
